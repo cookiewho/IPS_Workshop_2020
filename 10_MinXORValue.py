214f0e0ff0f03251bf69d8abc1935b4b75c74b63
cookiewho <dinkwho@gmail.com>
2020-08-10 12:33:29 -0700

    min xor value

---
+++ 10_MinXORValue.py
@@ -0,0 +1,8 @@
+def min_xor_value(num):
+  min_num = num[-1]
+  for x in range(len(num)-1):
+    if (num[x] ^ num[x+1]) < min_num:
+      min_num = num[x] ^ num[x+1]
+  return min_num
+
+print(min_xor_value([0, 2, 5, 7]))
