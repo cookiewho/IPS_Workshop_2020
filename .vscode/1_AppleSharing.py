dc4a1dc0dde3305c7f7921884c52c1bafc8a2910
cookiewho <dinkwho@gmail.com>
2020-08-24 23:18:41 -0700

    distributes apples to kids

---
+++ .vscode/1_AppleSharing.py
@@ -0,0 +1,14 @@
+# Read the numbers like this:
+students = int(input())
+apples = int(input())
+
+
+# Print the result with print()
+print(apples // students)
+print(apples % students)
+# Example of division, integer division and remainder:
+"""
+print(63 / 5)
+print(63 // 5)
+print(63 % 5)
+"""
