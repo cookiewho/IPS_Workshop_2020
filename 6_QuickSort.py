727715dca526db51df0cc5ae58318914ac097011
cookiewho <dinkwho@gmail.com>
2020-08-31 15:20:59 -0700

    modified quick sort

--- 6_QuickSort.py
+++ 6_QuickSort.py
@@ -3,7 +3,7 @@ def partition(arr, start, end):
   x = start-1
   pivot = arr[end]
   for y in range(start, end):
-    if arr[y] <= pivot:
+    if arr[y] < pivot:
       x+=1
       if x!=y:
         arr[x],arr[y] = arr[y],arr[x]
@@ -14,25 +14,7 @@ def quickSort(arr, start, end):
     part = partition(arr, start, end)
     quickSort(arr, start, part-1)
     quickSort(arr, part+1, end)
-'''
-def partition(arr,low,high): 
-    i = ( low-1 )         # index of smaller element 
-    pivot = arr[high]     # pivot 
-  
-    for j in range(low , high): 
-        if   arr[j] < pivot: 
-            i = i+1 
-            arr[i],arr[j] = arr[j],arr[i] 
-  
-    arr[i+1],arr[high] = arr[high],arr[i+1] 
-    return ( i+1 ) 
-def quickSort(arr,low,high): 
-    if low < high: 
-        pi = partition(arr,low,high) 
-        quickSort(arr, low, pi-1) 
-        quickSort(arr, pi+1, high)
-'''
-test = [10,30,80,90,40,50,70,100, 55, 20, 47, 69, 420]
+test = [420, 10,30,80,90,40,50,70,100, 55, 20, 47, 69]
 print(test)
 quickSort(test, 0, len(test)-1)
 print(test)
